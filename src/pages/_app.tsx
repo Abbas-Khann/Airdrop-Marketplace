919b6af72e085c0aa35c17053a6a7e577d2717a5
CJskii <czareczarek3@gmail.com>
2024-04-03 17:06:34 +0100

    Adding rainbowkit + wagmi + walletConnect button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,36 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
-
 import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { mainnet } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
+const queryClient = new QueryClient();
+
+const wagmiConfig = getDefaultConfig({
+  appName: process.env.NEXT_PUBLIC_WALLETCONNECT_APP_NAME || "",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+  chains: [mainnet],
+  ssr: true,
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
-      <Component {...pageProps} />
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </main>
   );
 };
