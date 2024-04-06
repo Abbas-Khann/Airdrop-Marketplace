fc8839f231ba56f3ce2da9b6a790254c253530b1
CJskii <czareczarek3@gmail.com>
2024-04-06 14:40:53 +0100

    adjust conditional queries

--- prisma/get/userData.ts
+++ prisma/get/userData.ts
@@ -1,16 +1,12 @@
-import { prisma } from "../../client";
+import { prisma } from "../client";
 
 interface getUserProps {
   ethAddress: string;
-  includeReferred?: boolean;
-  includeReferrals?: boolean;
   includeInteractions?: boolean;
   includeRewards?: boolean;
 }
 export const getUser = async ({
   ethAddress,
-  includeReferred = false,
-  includeReferrals = false,
   includeInteractions = false,
   includeRewards = false,
 }: getUserProps) => {
@@ -20,32 +16,6 @@ export const getUser = async ({
       ethereumAddress: true,
       id: true,
       inviteCode: true,
-      referred: includeReferred
-        ? {
-            select: {
-              id: true,
-              referrerId: true,
-              referredId: true,
-              pointsAwarded: true,
-              points: true,
-              createdAt: true,
-              updatedAt: true,
-            },
-          }
-        : undefined,
-      referrals: includeReferrals
-        ? {
-            select: {
-              id: true,
-              referrerId: true,
-              referredId: true,
-              pointsAwarded: true,
-              points: true,
-              createdAt: true,
-              updatedAt: true,
-            },
-          }
-        : undefined,
       interactions: includeInteractions
         ? {
             select: {
@@ -71,26 +41,4 @@ export const getUser = async ({
         : undefined,
     },
   });
-};
-
-export const getReferrerData = async ({
-  inviteCode,
-}: {
-  inviteCode: string;
-}) => {
-  const referrerData = await prisma.user.findUnique({
-    where: { inviteCode: inviteCode },
-    include: {
-      referrals: {
-        select: { id: true },
-      },
-    },
-  });
-
-  if (!referrerData) {
-    return { referrerId: null, referredCount: 0 };
-  }
-
-  const referredCount = referrerData.referrals.length;
-  return { referrerId: referrerData.id, referredCount };
 };
