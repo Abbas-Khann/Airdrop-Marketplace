d04e647a26390de0cb4c0c590f9689ae871b9bde
CJskii <czareczarek3@gmail.com>
2024-04-09 16:16:13 +0100

    add airdrop signup page

--- src/pages/airdrops/index.tsx
+++ src/pages/airdrops/index.tsx
@@ -1,10 +1,20 @@
 import DashboardLayout from "@/components/dashboard/layout";
 import AirdropsTable from "./airdrops-table";
+import AirdropsSignup from "./airdrop-signup";
 
 export default function AirdropHunterPage() {
+  // Here we will be checking if the user is logged in or not
+  // If the user is not logged in, we will ask to log in
+
+  // If the user is logged in, we will check if the user is eligible for AirdropsTable
+
+  // If the user is eligible, we will show AirdropsTable
+
+  // If the user is not eligible, we will show AirdropsSignup
   return (
     <DashboardLayout>
-      <AirdropsTable />
+      {/* <AirdropsTable /> */}
+      <AirdropsSignup />
     </DashboardLayout>
   );
 }
