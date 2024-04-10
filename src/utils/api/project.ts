75f0c1ce47bc6ce57c4f4e810f4d9226ab3ce16d
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-04-11 01:17:27 +0530

    feat: handlers added

---
+++ src/utils/api/project.ts
@@ -0,0 +1,109 @@
+import {
+  CategoryType,
+  DifficultyType,
+  LikelihoodType,
+  LinkType,
+} from "@prisma/client";
+
+export type ProjectDataType = {
+  id: number;
+  name: string;
+  shortDescription: string;
+  about?: string;
+  moreDescription?: string;
+  difficulty: DifficultyType;
+  category: CategoryType;
+  likelihood: LikelihoodType;
+  rating: number;
+  featured: boolean;
+  network: string;
+  createdAt: string;
+  updatedAt: string;
+  links?: {
+    id: number;
+    url: string;
+    type: LinkType;
+  }[];
+  task?: {
+    id: number;
+    name: string;
+    difficulty: DifficultyType;
+    about: string;
+    instructions: string;
+    steps?: {
+      id: number;
+      name: string;
+      description: string;
+    }[];
+  }[];
+}[];
+
+export async function getProject({
+  id,
+  name,
+}: {
+  id?: number;
+  name?: string;
+}): Promise<ProjectDataType | unknown> {
+  let response;
+  let error;
+  let url;
+  if (id) {
+    url = `api/project?id=${id}`;
+  } else if (name) {
+    url = `api/project?name=${name}`;
+  } else {
+    return;
+  }
+  try {
+    response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = (await response.json()) as ProjectDataType;
+    return data;
+  } catch (e) {
+    console.log(e);
+    error = e;
+    return error;
+  }
+}
+
+export type CreateProjectType = {
+  name: string;
+  shortDescription: string;
+  about: string;
+  moreDescription?: string;
+  difficulty: DifficultyType;
+  category: CategoryType;
+  likelihood: LikelihoodType;
+  rating: number;
+  featured: boolean;
+  network: string;
+  links?: { url: string; type: LinkType }[];
+};
+
+export async function createProject(projectData: CreateProjectType) {
+  let response;
+  let error;
+
+  try {
+    response = await fetch("api/project", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...projectData,
+      }),
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+    error = e;
+    return error;
+  }
+}
---
+++ src/utils/api/user.ts
@@ -0,0 +1,55 @@
+import { DifficultyType } from "@prisma/client";
+
+export type FavouriteProjectType = {
+  userId: number;
+  projectId: number;
+};
+
+export type CompleteTaskType = {
+  userId: number;
+  taskId: number;
+};
+
+export async function favouriteProject(data: FavouriteProjectType) {
+  let response;
+  let error;
+
+  try {
+    response = await fetch("api/user/project", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...data,
+      }),
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+    error = e;
+    return error;
+  }
+}
+
+export async function completeTask(data: CompleteTaskType) {
+  let response;
+  let error;
+
+  try {
+    response = await fetch("api/user/task", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...data,
+      }),
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+    error = e;
+    return error;
+  }
+}
